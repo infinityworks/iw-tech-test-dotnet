0f997414c42ae7f8c84660fdb6afb36d15dabbc8
Paul Lucas <paul.lucas@infinityworks.com>
2018-11-09 22:19:25 +0000

    Added ratings endpoint with test data

--- Controllers/RatingController.cs
+++ Controllers/RatingController.cs
@@ -38,5 +38,39 @@ namespace InfinityWorks.TechTest.Controllers
 
             return Json(authorityList);
         }
+
+        /// <summary>
+        /// Produces the ratings for a specific authority for the table
+        /// </summary>
+        /// <param name="authorityId">The authority to calculate ratings for</param>
+        /// <returns>
+        /// The ratings to display
+        /// </returns>
+        [HttpGet("{authorityId}")]
+        public JsonResult Get(int authorityId)
+        {
+            //This is just sample data to demonstrate the contract of the API
+            var ratings = new List<AuthorityRatingItem>();
+            if (authorityId == 1)
+            {
+                ratings.Add(new AuthorityRatingItem("5-star", 22.41));
+                ratings.Add(new AuthorityRatingItem("4-star", 43.13));
+                ratings.Add(new AuthorityRatingItem("3-star", 12.97));
+                ratings.Add(new AuthorityRatingItem("2-star", 1.54));
+                ratings.Add(new AuthorityRatingItem("1-star", 17.84));
+                ratings.Add(new AuthorityRatingItem("Exempt", 2.11));
+            }
+            else
+            {
+                ratings.Add(new AuthorityRatingItem("5-star", 50));
+                ratings.Add(new AuthorityRatingItem("4-star", 0));
+                ratings.Add(new AuthorityRatingItem("3-star", 0));
+                ratings.Add(new AuthorityRatingItem("2-star", 0));
+                ratings.Add(new AuthorityRatingItem("1-star", 25));
+                ratings.Add(new AuthorityRatingItem("Exempt", 25));
+            }
+
+            return Json(ratings);
+        }
     }
 }
